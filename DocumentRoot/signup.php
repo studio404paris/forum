b9e0ba2dea9be4e01048959fb14d24ffe601b4de
studio404paris <Simontaieb91@gmail.com>
2023-04-01 17:55:39 +0200

    Ajout de la methode et des names sur le form

--- DocumentRoot/signup.php
+++ DocumentRoot/signup.php
@@ -3,46 +3,36 @@
 <?php include 'includes/head.php'; ?>
 <body class="bg-dark">
     <section class="container">
-<<<<<<< HEAD
-        <div class="row m-5 p-5 bg-light arrondi_card ">
-=======
-        <div class="row m-5 p-5 bg-light arrondi_card animate__animated animate__bounceInDown">
->>>>>>> dev
+        <div class="row m-5  p-5 bg-light arrondi_card animate__animated animate__bounceInDown">
             <div class="mb-3 d-flex justify-content-center ">
                 <script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>
                 <lottie-player src="https://assets6.lottiefiles.com/datafiles/eAbFb2vqjxlD8oV/data.json"  background="transparent"  speed="1"  style="width: 150px; height: 150px;"    autoplay></lottie-player> 
             </div>
-<<<<<<< HEAD
-            <div class="mb-3 ">
-                <hr class="hr" />
-                <h1 class="text-center font-weight-bold ">Inscription</h1>
-=======
             <div class="mb-3 text-center ">
                 <hr class="hr" />
                 <h1 class="font-weight-bold ">Inscription</h1>
                 <p>Enregistrez vous pour pouvoir acceder à nos services</p>
->>>>>>> dev
                 <hr class="hr" />
-         
+        
             </div>
-            <form class="text-center">
+            <form class="text-center" method="POST">
                 <div class="mb-3 m-2">
                     <label for="exampleInputEmail1" class="form-label h5">Pseudo</label>
-                    <input type="text" class="form-control">
+                    <input type="text" class="form-control" name="pseudo">
                 </div>
                 <div class="mb-3 m-2">
                     <label for="exampleInputEmail1" class="form-label h5">Nom</label>
-                    <input type="text" class="form-control">
+                    <input type="text" class="form-control" name="lastName">
                 </div>
                 <div class="mb-3 m-2">
                     <label for="exampleInputEmail1" class="form-label h5">Prénom</label>
-                    <input type="text" class="form-control">
+                    <input type="text" class="form-control" name="firstName">
                 </div>
                 <div class="mb-3 m-2">
                     <label for="exampleInputPassword1" class="form-label h5">Mot de passe</label>
-                    <input type="password" class="form-control" >
+                    <input type="password" class="form-control" name="password">
                 </div>
-                <button type="submit" class="btn btn_valider m-2">S'inscrire</button>
+                <button type="submit" class="btn btn_valider m-2" name="validate">S'inscrire</button>
           </form>
         </div>
     </section>
