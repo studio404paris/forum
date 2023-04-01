48838deaa3898a407433134984ffdca4d8572f25
studio404paris <Simontaieb91@gmail.com>
2023-04-01 17:25:07 +0200

    Ajout des includes et stylisation du 1er form

---
+++ DocumentRoot/includes/head.php
@@ -0,0 +1,12 @@
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <link rel="stylesheet" href="/assets/css/style.css">
+    <link href="/assets/bootstrap/css/bootstrap.min.css" rel="stylesheet" >
+    <script src="/assets/bootstrap/js/bootstrap.bundle.min.js" ></script>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&family=Rubik:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+    <title>Forum | Xamis</title>
+</head>
